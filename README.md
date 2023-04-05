0189bdb79291d001ced77fee94228303ae8ea2f6
ermurray <eric@ermurray.ca>
2023-04-04 19:45:00 -0600

    post lecture

--- README.md
+++ README.md
@@ -2,49 +2,34 @@
 LightHouse labs  web flex lecture  on HTTP cookies and user Authentication
 [Lecture Repo](https://github.com/ermurray/cookies-userAuth) | [Lecture Recording](https://vimeo.com/751948814/612b284a02)
 ## Topics
-- [x] HTTP review
-- [x] HTTP cookies
-- [x] Build small express ejs login
-- [x] Questions / concerns
+- [ ] HTTP review
+- [ ] HTTP cookies
+- [ ] Build small express ejs login
+- [ ] Questions / concerns
 
 ### HTTP review
 - request response style protocol
-- it is stateless
+- why is HTTP stateless
+  -scalability
+    - make it bigger/better (vertical)
+    - make more (horizontal)
+  -complexity
+    - not tracking sessions etc.
+  - easier caching
+    - track  unchanged data easily
+  -server crashes etc. 
 
-  ```
-  The HTTP protocol is a stateless one. This means that every HTTP request the server receives is independent and does not relate to requests that came prior to it.
-  
-  ie. a request is made for the first ten user records, then another request is made for the next ten records.
-
-  On a stateful protocol, the server remembers each client position inside the result-set, and therefore the requests will be similar to:
-
-  Give me the first ten user records
-  Give me the next ten records
-  On a stateless protocol, the requests will be a bit different. The server doesn't hold the state of its client, and therefore the client's position in the result-set needs to be sent as part of the requests: ...
-  ```
-  - why is it stateless
-    - scalability
-      - make it bigger/better (vertical)
-      - make more (horizontal)
-    - complexity
-      - not tracking sessions etc.
-    - easier caching
-      - track unchanged data easily
-    - server doesn't loose state on restart crash etc.
-
-  - what does this mean
-    - doesnt keep track of context and must be provided every time
-    - we need a cookie or token some way to manage session state
-
+  - what are the trade offs
+    - must re do steps every time (unless cached)
+    - we need to store info between requests, a way maintain sessions and session state
 ### HTTP cookies
 - cookies:
-  - Allow us to store user info between HTTP request
-  - stored as key value pair
-    - "user_id" : "12388d77dds"  
-  - stored by the browser
+  - allows us to save some state between requests
+  - stored as key/value pairs
+    - "user_id" : "1232ssry129"
+  - stored only on browser
   - three main purposes
-    - session 
+    - session
     - personalization
     - tracking
-  - set on the response object
-  - provided in the request to the server 
+  -
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
+const PORT = 3000;
 
 const app = express();
-const PORT = 3000;
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
@@ -11,9 +11,8 @@ app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
-app.listen(PORT, () => console.log(`My fancy server is listening on port: ${PORT}`));
+app.listen(PORT, () => console.log(`My Fancy Cookie Server is listening on port: ${PORT}`));
 
-// will not persist between server restarts
 const users = {
   123: {
     id: '123',
@@ -24,33 +23,54 @@ const users = {
   234: {
     id: '234',
     name: 'Bender',
-    email:'bender@robot.com',
+    email: 'bender@robot.com',
     password: 'cigar'
   }
 }
 
 const findUserByEmail = (email) => {
   for(const userId in users) {
-    if (users[userId].email === email) {
+    if(users[userId].email === email){
       return users[userId]
     }
   }
+  return null;
 };
 
-app.get('/', (req, res) => { 
-  res.send('hey im here listening');
+
+app.get('/', (req, res) => {
+  console.log('hey im here in root');
+
+  res.render('index', {cookies: null});
 });
 
 app.get('/home', (req, res) => {
-  console.log('cookies obj =============>',req.cookies)
-  // const templateVars =
-  
-  res.render('index',  {cookies: req.cookies});
+  console.log('cookies obj ======>', req.cookies);
+
+  res.render('index', {cookies: req.cookies})
 });
 
 app.get('/login', (req, res) => {
   res.render('login');
 });
+
+app.get('/protected', (req, res) => {
+  const user = req.cookies.user;
+  console.log('user', user);
+
+  if (!user) {
+    return res.status(401).send("you cant be here");
+  }
+  const dbUser = users[user.id]
+
+  console.log('dbUser', dbUser);
+  if (!dbUser) {
+    return res.status(401).send('Invalid cookie');
+  }
+
+  res.render('protected');
+});
+
 
 app.post('/login', (req, res) => {
   console.log('req.body', req.body);
@@ -58,41 +78,22 @@ app.post('/login', (req, res) => {
   const password = req.body.password;
 
   if (!email || !password) {
-    return res.status(400).send("email and password cannot be blank");
+    return res.status(400).send("email and/or password can not be blank")
   }
-
+  
   const user = findUserByEmail(email);
   console.log('user obj', user);
 
-  if(!user){
-    return res.status(400).send("a user with that email does not exist");
+  if(user.password !== password){
+    return res.status(400).send("password does not match")
   }
 
-  if(user.password !== password) {
-    return res.status(400).send('password does not match');
-  }
-
-  res.cookie('user', {id: user.id, name: user.name});
-  res.redirect('/home');
-});
-
-app.get('/protected', (req, res) => {
-  const user = req.cookies.user;
-
-  if(!user) {
-    return res.status(401).send("Unauthorized");
-  }
-
-  const database_user = users[user.id];
-
-  if(!database_user) {
-    return res.status(401).send("Invalid Cookie");
-  }
-
-  res.render('protected');
+  res.cookie('user', {id: user.id, name: user.name})
+  res.redirect('/home')
 });
 
 app.post('/logout', (req, res) => {
+  // clear our cookies so user appears logged out
   res.clearCookie('user');
-  res.redirect('/login');
+  res.redirect('/login')
 });
